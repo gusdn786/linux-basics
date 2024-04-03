ed462d8f43fdf9857ea24d48bfab9188ce5ef77f
gusdn786 <gusdn786@naver.com>
2024-04-03 16:38:23 +0900

    quiz1~quiz10 까지

---
+++ basic/quiz7.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+touch notes.txt
+cat > notes.txt
+"Hello, Linux!"
+cat notes.txt
---
+++ basic/quiz9.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+touch multiLine.txt
+cat > multiLine.txt
+\n
+\n
+\n
+\n
+\n
+\n
+\n
+head -n 3 multiLine.txt
